b706da8d38027c8f5c80417241e40e984fca3086
Alex Zinchenko <yumitsu@users.noreply.github.com>
2019-09-27 00:22:32 +0300

    Fixed shasum

--- dte.rb
+++ dte.rb
@@ -2,7 +2,7 @@ class Dte < Formula
   desc "Small and configurable console text editor"
   homepage "https://github.com/craigbarnes/dte"
   url "https://github.com/craigbarnes/dte/releases/download/v1.9/dte-1.9.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "79510df2b9e7039f33976ca0e0040f98493e9c2bfecdfbcd0bb39cbe54468788"
 
   def install
     system "make", "-j#{ENV.make_jobs}"
