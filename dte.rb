aaf4b3cfb091e0cf37dd9940a3bee4f069c0fd56
Alex Zinchenko <yumitsu@users.noreply.github.com>
2019-11-02 20:15:51 +0300

    Version bump

--- dte.rb
+++ dte.rb
@@ -1,8 +1,8 @@
 class Dte < Formula
   desc "Small and configurable console text editor"
   homepage "https://github.com/craigbarnes/dte"
-  url "https://github.com/craigbarnes/dte/releases/download/v1.9/dte-1.9.tar.gz"
-  sha256 "79510df2b9e7039f33976ca0e0040f98493e9c2bfecdfbcd0bb39cbe54468788"
+  url "https://github.com/craigbarnes/dte/archive/v1.9.1.tar.gz"
+  sha256 "80d2732269a308b5e1126ecc16c28cda032864f625a95184821a73c054f81a2d"
 
   def install
     system "make", "-j#{ENV.make_jobs}"
